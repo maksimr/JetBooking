d73e9be2d38b8bd157e91a79c863271419626623
Maksim Ryzhikov <rv.maksim@gmail.com>
2018-08-19 01:11:02 +0300

    refactor: extract vc api test helpers to separate file

--- test/api/vc_test.dart
+++ test/api/vc_test.dart
@@ -1,9 +1,7 @@
-import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 import 'package:test/test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:jetbooking/api/vc.dart';
+import 'vc.dart';
 
 void main() {
   test('should return list of rooms', () {
@@ -32,42 +30,3 @@ void main() {
                 1000}"), [createRoomMock()]));
   });
 }
-
-whenGetUrl(url, responseData) {
-  final MockHttpClient client = new MockHttpClient();
-
-  when(client.getUrl(url)).thenAnswer((Invocation _) {
-    final MockHttpClientRequest request = new MockHttpClientRequest();
-    final MockHttpClientResponse response = new MockHttpClientResponse();
-    when(request.close()).thenAnswer((_) => Future.value(response));
-    when(response.transform(any)).thenAnswer((invocation) {
-      final decoder = invocation.positionalArguments[0];
-      return Stream
-          .fromFuture<List<int>>(
-              Future.value(utf8.encode(json.encode(responseData))))
-          .transform(decoder);
-    });
-    return Future.value(request);
-  });
-
-  return client;
-}
-
-createRoomMock() {
-  return {
-    "id": "KQPjltJIA6GHhSLfLG2fRT",
-    "title": "San Francisco-113",
-    "description": "Number (H.232): 22032",
-    "location": "San Francisco",
-    "locationId": "-X4KZApLfg.CvpYyK89qVE",
-    "floor": 1,
-    "capacity": 10,
-    "hasTv": true
-  };
-}
-
-class MockHttpClient extends Mock implements HttpClient {}
-
-class MockHttpClientRequest extends Mock implements HttpClientRequest {}
-
-class MockHttpClientResponse extends Mock implements HttpClientResponse {}

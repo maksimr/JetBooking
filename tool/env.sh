169a785f49938e7f072a81be66a9a13f4f20a400
Maksim Ryzhikov <rv.maksim@gmail.com>
2018-09-11 15:34:43 +0300

    chore: add setup flutter script

---
+++ tool/env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export FLUTTER_ROOT=$(dirname ${BASH_SOURCE[0]:-$0})/flutter
+export PATH="$FLUTTER_ROOT/bin:$PATH"
---
+++ tool/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+pushd "$(dirname ${BASH_SOURCE[0]:-$0})"
+if [ ! -d flutter ]; then
+  git clone https://github.com/flutter/flutter.git -b beta --depth 1
+fi
+
+source ./env.sh
+flutter doctor

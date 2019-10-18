43d4ecc44e79a3f45b88517f4fbb0bd061a60fba
Maksim Ryzhikov <rv.maksim@gmail.com>
2019-10-18 21:43:05 +0300

    chore: move install dependency step to sh script

---
+++ tool/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+case "$(uname -s)" in
+  Darwin*)
+    export HOMEBREW_NO_AUTO_UPDATE=1
+    brew install libimobiledevice
+    brew install ideviceinstaller
+    brew install ios-deploy
+    pip install six
+    ;;
+  *)
+    # Flutter depends on /usr/lib/x86_64-linux-gnu/libstdc++.so.6
+    # version GLIBCXX_3.4.18
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+    sudo apt-get update
+    sudo apt-get install libstdc++6
+    ;;
+esac
